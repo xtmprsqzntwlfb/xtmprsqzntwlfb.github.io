cba42fe8fe47fba41d042e3e50b80512ed8b6e27
Tatiana Ovchinnikova <t.v.ovtchinnikova@gmail.com>
2026-06-23 14:28:08 -0500

    Transform site into technical blog with performance optimizations

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-(function ($) {
-  $(window).on('load hashchange', function(){
-    $('.content-region').hide();
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    $(region).show();
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
-  });
-  
-})(jQuery);
+(function() {
+  function showRegion() {
+    // Hide all content regions
+    document.querySelectorAll('.content-region').forEach(function(el) {
+      el.classList.remove('show');
+      el.classList.add('hide');
+    });
+
+    // Remove active class from all menu items
+    document.querySelectorAll('.main-menu a').forEach(function(el) {
+      el.classList.remove('active');
+    });
+
+    // Get the current hash or default to first menu item
+    var region = location.hash || document.querySelector('.main-menu a').getAttribute('href');
+
+    // Show the selected region
+    var targetRegion = document.querySelector(region);
+    if (targetRegion) {
+      targetRegion.classList.remove('hide');
+      targetRegion.classList.add('show');
+    }
+
+    // Add active class to current menu item
+    var activeLink = document.querySelector('.main-menu a[href="' + region + '"]');
+    if (activeLink) {
+      activeLink.classList.add('active');
+    }
+  }
+
+  // Run on load and hash change
+  window.addEventListener('load', showRegion);
+  window.addEventListener('hashchange', showRegion);
+})();
